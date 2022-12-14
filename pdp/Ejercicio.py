c26faa38b488e0940acadd7ed177517ff33818ce
Airtoc <dark242lol@gmail.com>
2022-12-14 14:46:15 -0500

    pdp

---
+++ pdp/Ejercicio.py
@@ -0,0 +1,41 @@
+from stochasticdp import StochasticDP
+
+# Número de etapas
+number_of_stages = 4
+
+# Lista de estados
+states = [0, 1]
+
+# Lista de decisiones
+decisions = [0, 1, 2, 3, 4, 5]
+
+# Inicializar programa
+dp = StochasticDP(number_of_stages, states, decisions, minimize=True)
+
+# Probabilidades de transición y contribuciones del estado n = 0
+for t in range(number_of_stages - 1):
+    for x in decisions:
+        dp.probability[1, 0, t, x] = 0
+        dp.contribution[1, 0, t, x] = 0
+        dp.probability[0, 0, t, x] = 1
+        dp.contribution[0, 0, t, x] = 0
+
+# Probabilidades de transición y contribuciones del estado n = 1
+for t in range(number_of_stages - 1):
+    for x in decisions:
+        if x > 0:
+            K = 3
+        else:
+            K = 0
+        dp.probability[0, 1, t, x] = 1 - (1/2)**x
+        dp.contribution[0, 1, t, x] = K + x
+        dp.probability[1, 1, t, x] = (1/2)**x
+        dp.contribution[1, 1, t, x] = K + x
+
+# Condiciones iniciales
+dp.boundary[0] = 0
+dp.boundary[1] = 16
+
+# Resolver 
+value, policy = dp.solve()
+print(value)
